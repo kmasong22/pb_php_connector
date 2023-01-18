aacf2a2ecd895269dca9f3b86d3d6b9670d785df
Kerwin Masong <masong.wfm@gmail.com>
2023-01-18 08:23:07 +0800

    Uploaded updated file

--- pb.php
+++ pb.php
@@ -5,23 +5,29 @@
 if( !defined( 'API_SERVER' ) ){
 	define( 'API_SERVER', 'http://127.0.0.1:8090' );
 }
+if( !defined( 'API_PREFIX' ) ){
+	define( 'API_PREFIX', '' );
+}
 
 class PB{
 
 	public static $_URL = API_SERVER ;
+	public static $_API_PREFIX = API_PREFIX ;
 	public static $headers = array() ;
 	public static $headerx = array() ;
 	public static $collection ;
 
+	// Initiates the record
   public static function _init($name = 'users') {
     self::$collection = $name ;
-    $authorization = isset($_SESSION['authStore_token']) ? $_SESSION['authStore_token'] : '' ;
-    self::$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $authorization );
+    $authorization = isset($_SESSION[self::$_API_PREFIX.'_authStore_token']) ? $_SESSION[self::$_API_PREFIX.'_authStore_token'] : '' ;
+    self::$headers = array( 'Content-Type: application/json', 'Authorization: Bearer ' . $authorization );
 		self::$headerx = array( 'Authorization: Bearer ' . $authorization );
   }
 
+	// Get a record
 	public static function GET( $id, $_collection = '' ){
-
+		self::_init( $_collection );
 		$_collection = $_collection == '' ? self::$collection : $_collection ;
 
 		$request = "/api/collections/".$_collection."/records/{$id}";
@@ -39,8 +45,9 @@ class PB{
 
 	}
 
+	// List records
 	public static function LIST( $post = [], $_collection = '' ){
-
+		self::_init( $_collection );
 		$_collection = $_collection == '' ? self::$collection : $_collection ;
 
 		$request = "/api/collections/".$_collection."/records" . '?' . http_build_query($post) ;
@@ -57,8 +64,10 @@ class PB{
 		return $result ;
 
 	}
-	public static function CREATE( $post, $_collection = '' ){
 
+	// Create a record
+	public static function CREATE( $post, $_collection = '' ){
+		self::_init( $_collection );
 		$_collection = $_collection == '' ? self::$collection : $_collection ;
 
 		$request = "/api/collections/".$_collection."/records";
@@ -78,8 +87,9 @@ class PB{
 
 	}
 
+	// Updates a record
 	public static function UPDATE( $id, $post, $_collection = '' ){
-
+		self::_init( $_collection );
 		$_collection = $_collection == '' ? self::$collection : $_collection ;
 
 		$request = "/api/collections/".$_collection."/records/{$id}";
@@ -99,32 +109,41 @@ class PB{
 
 	}
 
+	// Authenticate session
 	public static function AUTHENTICATE( $identity, $password ){
 
 		$request = "/api/collections/users/auth-with-password";
-		$data = array( 'identity' => $identity, "password" => $password );
+		$post = array( 'identity' => $identity, "password" => $password );
 		
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, self::$_URL.$request );
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true );
-		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode( $data ) );
-		curl_setopt($ch, CURLOPT_HTTPHEADER, self::$headers );
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post );
+		// curl_setopt($ch, CURLOPT_HTTPHEADER, self::$headers );
 		$response = curl_exec($ch);
 		$result = json_decode($response);
 
 		curl_close($ch);
 		
 		if(isset($result->token)){
-			$_SESSION['authStore_token'] = $result->token ;
+			$_SESSION[self::$_API_PREFIX.'_authStore_token'] = $result->token ;
+			$_SESSION[self::$_API_PREFIX.'_userid'] = $result->record->id ;
+			$_SESSION[self::$_API_PREFIX.'_username'] = $result->record->username ;
+			$_SESSION[self::$_API_PREFIX.'_email'] = $result->record->email ;
+			$_SESSION[self::$_API_PREFIX.'_name'] = $result->record->name ;
+			$_SESSION[self::$_API_PREFIX.'_avatar'] = $result->record->avatar ;
+			$_SESSION[self::$_API_PREFIX.'_role'] = $result->record->role ;
+			$_SESSION[self::$_API_PREFIX.'_verified'] = $result->record->verified ;
 		}
 
 		return $result ;
 
 	}
 
-	public static function DELETE( $id ){
-
+	// Delete a record
+	public static function DELETE( $id, $_collection = '' ){
+		self::_init( $_collection );
 		$request = "/api/collections/".self::$collection."/records/{$id}";
 		
 		$ch = curl_init();
@@ -140,9 +159,14 @@ class PB{
 		return $result ;
 	}
 
-	public static function QUIT(){
+	// Unsets the session
+	public static function QUIT( ){
 
-		unset($_SESSION['authStore_token']);
+		foreach($_SESSION as $key => $value){
+			if (strpos($key, self::$_API_PREFIX.'_') === 0){
+				unset($_SESSION[$key]); 
+			}
+		}
 
 	}
 
